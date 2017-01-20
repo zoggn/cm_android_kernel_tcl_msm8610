f890302f89fd533f5f960d4a0663382903a6393a
zoggn <kurkov.nikita@gmail.com>
2017-01-20 14:05:48 +0200

    update goodix touchpanel for miata

--- drivers/input/touchscreen/gt9xx/gt9xx.h
+++ drivers/input/touchscreen/gt9xx/gt9xx.h
@@ -25,9 +25,19 @@
 
 #include <linux/kernel.h>
 #include <linux/i2c.h>
+#include <linux/irq.h>
+#include <linux/input.h>
+#include <linux/slab.h>
+#include <linux/interrupt.h>
 #include <linux/delay.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
 #include <linux/gpio.h>
-#include <linux/uaccess.h>
+#include <linux/proc_fs.h>
+#include <linux/regulator/consumer.h>
+#include <linux/firmware.h>
+#include <linux/debugfs.h>
+#include <asm/uaccess.h>
 
 #if defined(CONFIG_FB)
 #include <linux/notifier.h>
@@ -36,18 +46,13 @@
 #include <linux/earlysuspend.h>
 #define GOODIX_SUSPEND_LEVEL 1
 #endif
-
-#define MAX_BUTTONS 4
-#define GOODIX_MAX_CFG_GROUP	6
-#define GTP_FW_NAME_MAXSIZE	50
-
+#define  GT_VIRTUAL_KEY 1
 struct goodix_ts_platform_data {
 	int irq_gpio;
 	u32 irq_gpio_flags;
 	int reset_gpio;
 	u32 reset_gpio_flags;
-	const char *product_id;
-	const char *fw_name;
+	u32 family_id;
 	u32 x_max;
 	u32 y_max;
 	u32 x_min;
@@ -58,11 +63,18 @@ struct goodix_ts_platform_data {
 	u32 panel_maxy;
 	bool no_force_update;
 	bool i2c_pull_up;
-	bool enable_power_off;
-	size_t config_data_len[GOODIX_MAX_CFG_GROUP];
-	u8 *config_data[GOODIX_MAX_CFG_GROUP];
-	u32 button_map[MAX_BUTTONS];
-	u8 num_button;
+	int gtp_cfg_len;
+	u8 *config_data;
+#if GT_VIRTUAL_KEY
+	//const char *name;
+	int disp_maxx;
+	int disp_maxy;
+	int pan_maxx;
+	int pan_maxy;
+	int *keycodes;
+	int num_keys;
+	int y_offset;
+#endif
 };
 struct goodix_ts_data {
 	spinlock_t irq_lock;
@@ -72,8 +84,6 @@ struct goodix_ts_data {
 	struct hrtimer timer;
 	struct workqueue_struct *goodix_wq;
 	struct work_struct	work;
-	char fw_name[GTP_FW_NAME_MAXSIZE];
-	struct delayed_work goodix_update_work;
 	s32 irq_is_disabled;
 	s32 use_irq;
 	u16 abs_x_max;
@@ -90,8 +100,6 @@ struct goodix_ts_data {
 	u8  fixed_cfg;
 	u8  esd_running;
 	u8  fw_error;
-	bool power_on;
-	struct mutex lock;
 	struct regulator *avdd;
 	struct regulator *vdd;
 	struct regulator *vcc_i2c;
@@ -100,7 +108,6 @@ struct goodix_ts_data {
 #elif defined(CONFIG_HAS_EARLYSUSPEND)
 	struct early_suspend early_suspend;
 #endif
-	struct dentry *debug_base;
 };
 
 extern u16 show_len;
@@ -111,115 +118,181 @@ extern u16 total_len;
 #define GTP_CHANGE_X2Y			0
 #define GTP_DRIVER_SEND_CFG		1
 #define GTP_HAVE_TOUCH_KEY		1
-#define GTP_ESD_PROTECT			0
+#define GTP_POWER_CTRL_SLEEP	0
+#define VER_READ 				1//add the attribute for reading the version of firmware 2013-12-03-Richard  
+
+/* auto updated by .bin file as default */
+#define GTP_AUTO_UPDATE			0
+/* auto updated by head_fw_array in gt9xx_firmware.h,
+ * function together with GTP_AUTO_UPDATE */
+#define GTP_HEADER_FW_UPDATE	1
+
+#define GTP_CREATE_WR_NODE		1
+#define GTP_ESD_PROTECT			1// open the flag for ESD testing ---Richard---2013/11/26
 #define GTP_WITH_PEN			0
 
-/* This cannot work when enable-power-off is on */
 #define GTP_SLIDE_WAKEUP		0
 /* double-click wakeup, function together with GTP_SLIDE_WAKEUP */
 #define GTP_DBL_CLK_WAKEUP		0
 
-#define GTP_IRQ_TAB            {\
+#define GTP_DEBUG_ON			0
+#define GTP_DEBUG_ARRAY_ON		0
+#define GTP_DEBUG_FUNC_ON		0
+
+/*************************** PART2:TODO define *******************************/
+/* STEP_1(REQUIRED): Define Configuration Information Group(s) */
+/* Sensor_ID Map: */
+/* sensor_opt1 sensor_opt2 Sensor_ID
+ *	GND			GND			0
+ *	VDDIO		GND			1
+ *	NC			GND			2
+ *	GND			NC/300K		3
+ *	VDDIO		NC/300K		4
+ *	NC			NC/300K		5
+*/
+/* Define your own default or for Sensor_ID == 0 config here */
+/* The predefined one is just a sample config,
+ * which is not suitable for your tp in most cases. */
+ /* chang the config into version 68 for ESD testing and performance --- bug 619717 ---qifu.cheng added --- 14/3/12*/
+/*ADD--- add the version 71 of cofiguration for double click function --- bug 630236---qifu.cheng---04/22/14 */
+#define CTP_CFG_GROUP1 {\
+0x47,0x1C,0x02,0xC0,0x03,0x05,0x34,0x01,0x02,0x0F,\
+0x23,0x08,0x50,0x3C,0x03,0x05,0x00,0x00,0x00,0x00,\
+0x12,0x00,0x00,0x14,0x14,0x1C,0x14,0x89,0x08,0x0B,\
+0x37,0x00,0xB8,0x08,0x00,0x00,0x00,0x9A,0x33,0x1D,\
+0x00,0x11,0x02,0x00,0x00,0x00,0x00,0x00,0x00,0x00,\
+0x00,0x2C,0x6E,0x94,0xD5,0x02,0x05,0x00,0x00,0x04,\
+0x8F,0x30,0x00,0x7A,0x3A,0x00,0x6A,0x45,0x00,0x5C,\
+0x53,0x00,0x52,0x64,0x00,0x52,0x10,0x30,0x50,0x00,\
+0xF0,0x80,0x50,0xFF,0xFF,0x27,0x60,0x50,0x33,0x00,\
+0x04,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,\
+0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,\
+0x00,0x00,0x02,0x04,0x06,0x08,0x0A,0x0C,0x0E,0x10,\
+0x12,0x14,0x16,0xFF,0xFF,0xFF,0x00,0x00,0x00,0x00,\
+0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,\
+0x00,0x00,0x21,0x20,0x1F,0x1E,0x1D,0x1C,0x18,0x16,\
+0x12,0x10,0x0F,0x0A,0x08,0x06,0x04,0x02,0x00,0xFF,\
+0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0x00,0x00,\
+0x00,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,\
+0xFF,0xFF,0xFF,0xFF,0xF2,0x01\
+}
+
+/* Define your config for Sensor_ID == 1 here, if needed */
+#define CTP_CFG_GROUP2 {\
+	}
+
+/* Define your config for Sensor_ID == 2 here, if needed */
+#define CTP_CFG_GROUP3 {\
+	}
+
+/* Define your config for Sensor_ID == 3 here, if needed */
+#define CTP_CFG_GROUP4 {\
+	}
+
+/* Define your config for Sensor_ID == 4 here, if needed */
+#define CTP_CFG_GROUP5 {\
+	}
+
+/* Define your config for Sensor_ID == 5 here, if needed */
+#define CTP_CFG_GROUP6 {\
+	}
+
+#define GTP_IRQ_TAB		{\
 				IRQ_TYPE_EDGE_RISING,\
 				IRQ_TYPE_EDGE_FALLING,\
 				IRQ_TYPE_LEVEL_LOW,\
 				IRQ_TYPE_LEVEL_HIGH\
 				}
+#define GTP_GPIO_GET_VALUE(pin)         gpio_get_value(pin)
+#define GTP_GPIO_OUTPUT(pin,level)      gpio_direction_output(pin,level)
+#define GTP_GPIO_REQUEST(pin, label)    gpio_request(pin, label)
+#define GTP_GPIO_FREE(pin)              gpio_free(pin)
 
-
+/* STEP_3(optional): Specify your special config info if needed */
 #define GTP_IRQ_TAB_RISING	0
 #define GTP_IRQ_TAB_FALLING	1
 #if GTP_CUSTOM_CFG
-#define GTP_MAX_HEIGHT		800
-#define GTP_MAX_WIDTH		480
-#define GTP_INT_TRIGGER		GTP_IRQ_TAB_RISING
+  #define GTP_MAX_HEIGHT   960	
+  #define GTP_MAX_WIDTH    540
+  #define GTP_INT_TRIGGER  1    //0:Rising 1:Falling
 #else
 #define GTP_MAX_HEIGHT		4096
 #define GTP_MAX_WIDTH		4096
 #define GTP_INT_TRIGGER		GTP_IRQ_TAB_FALLING
 #endif
 
-#define GTP_PRODUCT_ID_MAXSIZE	5
-#define GTP_PRODUCT_ID_BUFFER_MAXSIZE	6
-#define GTP_FW_VERSION_BUFFER_MAXSIZE	4
-#define GTP_MAX_TOUCH		5
-#define GTP_ESD_CHECK_CIRCLE	2000      /* jiffy: ms */
+#define GTP_MAX_TOUCH         5
+ /* chang the  time of respond in ESD  --- bug 619717 ---qifu.cheng added --- 14/3/12*/
+#define GTP_ESD_CHECK_CIRCLE  1000      /* jiffy: ms *///richard
 
 /***************************PART3:OTHER define*********************************/
-#define GTP_DRIVER_VERSION	"V1.8.1<2013/09/01>"
-#define GTP_I2C_NAME		"Goodix-TS"
-#define GTP_POLL_TIME		10     /* jiffy: ms*/
-#define GTP_ADDR_LENGTH		2
+#define GTP_DRIVER_VERSION		"V1.8<2013/06/08>"
+#define GTP_I2C_NAME			"Goodix-TS"
+#define GTP_POLL_TIME			10     /* jiffy: ms*/
+#define GTP_ADDR_LENGTH			2
 #define GTP_CONFIG_MIN_LENGTH	186
 #define GTP_CONFIG_MAX_LENGTH	240
-#define FAIL			0
-#define SUCCESS			1
-#define SWITCH_OFF		0
-#define SWITCH_ON		1
+#define FAIL					0
+#define SUCCESS					1
+#define SWITCH_OFF				0
+#define SWITCH_ON				1
 
 /* Registers define */
-#define GTP_READ_COOR_ADDR	0x814E
-#define GTP_REG_SLEEP		0x8040
-#define GTP_REG_SENSOR_ID	0x814A
-#define GTP_REG_CONFIG_DATA	0x8047
-#define GTP_REG_FW_VERSION	0x8144
-#define GTP_REG_PRODUCT_ID	0x8140
+#define GTP_READ_COOR_ADDR		0x814E
+#define GTP_REG_SLEEP			0x8040
+#define GTP_REG_SENSOR_ID		0x814A
+#define GTP_REG_CONFIG_DATA		0x8047
+#define GTP_REG_VERSION			0x8140
 
-#define GTP_I2C_RETRY_3		3
-#define GTP_I2C_RETRY_5		5
-#define GTP_I2C_RETRY_10	10
-
-#define RESOLUTION_LOC		3
-#define TRIGGER_LOC		8
-
-/* HIGH: 0x28/0x29, LOW: 0xBA/0xBB */
-#define GTP_I2C_ADDRESS_HIGH	0x14
-#define GTP_I2C_ADDRESS_LOW	0x5D
+#define RESOLUTION_LOC			3
+#define TRIGGER_LOC				8
 
 #define CFG_GROUP_LEN(p_cfg_grp) (sizeof(p_cfg_grp) / sizeof(p_cfg_grp[0]))
+/* Log define */
+#define GTP_INFO(fmt,arg...)           printk("<<-GTP-INFO->> "fmt"\n",##arg)
+#define GTP_ERROR(fmt,arg...)          printk("<<-GTP-ERROR->> "fmt"\n",##arg)
+#define GTP_DEBUG(fmt, arg...)	do {\
+		if (GTP_DEBUG_ON) {\
+			pr_debug("<<-GTP-DEBUG->> [%d]"fmt"\n",\
+				__LINE__, ##arg); } \
+		} while (0)
 
-/* GTP CM_HEAD RW flags */
-#define GTP_RW_READ			0
-#define GTP_RW_WRITE			1
-#define GTP_RW_READ_IC_TYPE		2
-#define GTP_RW_WRITE_IC_TYPE		3
-#define GTP_RW_FILL_INFO		4
-#define GTP_RW_NO_WRITE			5
-#define GTP_RW_READ_ERROR		6
-#define GTP_RW_DISABLE_IRQ		7
-#define GTP_RW_READ_VERSION		8
-#define GTP_RW_ENABLE_IRQ		9
-#define GTP_RW_ENTER_UPDATE_MODE	11
-#define GTP_RW_LEAVE_UPDATE_MODE	13
-#define GTP_RW_UPDATE_FW		15
-#define GTP_RW_CHECK_RAWDIFF_MODE	17
+#define GTP_DEBUG_ARRAY(array, num)    do {\
+		s32 i; \
+		u8 *a = array; \
+		if (GTP_DEBUG_ARRAY_ON) {\
+			pr_debug("<<-GTP-DEBUG-ARRAY->>\n");\
+			for (i = 0; i < (num); i++) { \
+				pr_debug("%02x   ", (a)[i]);\
+				if ((i + 1) % 10 == 0) { \
+					pr_debug("\n");\
+				} \
+			} \
+			pr_debug("\n");\
+		} \
+	} while (0)
 
-/* GTP need flag or interrupt */
-#define GTP_NO_NEED			0
-#define GTP_NEED_FLAG			1
-#define GTP_NEED_INTERRUPT		2
+#define GTP_DEBUG_FUNC()	do {\
+	if (GTP_DEBUG_FUNC_ON)\
+		pr_debug("<<-GTP-FUNC->> Func:%s@Line:%d\n",\
+					__func__, __LINE__);\
+	} while (0)
 
+#define GTP_SWAP(x, y)		do {\
+					typeof(x) z = x;\
+					x = y;\
+					y = z;\
+				} while (0)
 /*****************************End of Part III********************************/
 
 void gtp_esd_switch(struct i2c_client *client, int on);
 
-int gtp_i2c_read_dbl_check(struct i2c_client *client, u16 addr,
-					u8 *rxbuf, int len);
-int gtp_send_cfg(struct goodix_ts_data *ts);
-void gtp_reset_guitar(struct goodix_ts_data *ts, int ms);
-void gtp_irq_disable(struct goodix_ts_data *ts);
-void gtp_irq_enable(struct goodix_ts_data *ts);
-
-#ifdef CONFIG_GT9XX_TOUCHPANEL_DEBUG
-s32 init_wr_node(struct i2c_client *client);
-void uninit_wr_node(void);
+#if GTP_CREATE_WR_NODE
+extern s32 init_wr_node(struct i2c_client *client);
+extern void uninit_wr_node(void);
 #endif
 
-#ifdef CONFIG_GT9XX_TOUCHPANEL_UPDATE
+#if GTP_AUTO_UPDATE
 extern u8 gup_init_update_proc(struct goodix_ts_data *ts);
-s32 gup_enter_update_mode(struct i2c_client *client);
-void gup_leave_update_mode(struct i2c_client *client);
-s32 gup_update_proc(void *dir);
-extern struct i2c_client  *i2c_connect_client;
 #endif
 #endif /* _GOODIX_GT9XX_H_ */
